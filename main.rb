720fdce080562e28b0071ab594785a8d2bbe3162
moffire <alexeydudarev.spb@gmail.com>
2019-11-12 17:23:55 +0300

    Added buttons(with empty method)

--- main.rb
+++ main.rb
@@ -1,17 +1,22 @@
 require './secret.rb'
 require 'telegram/bot'
 
+def rutor
+  nil
+end
 
 Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
+
+    buttons = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: [%w(Rutor Empty_1), %w(Empty_2 Искать_везде)], resize_keyboard: true)
+
     case message.text
     when '/start'
-      bot.api.send_message(
-          chat_id: message.chat.id,
-          text:    "Hello, #{message.from.first_name}"
-      )
+      bot.api.send_message(chat_id: message.chat.id, text: 'Выберите трекер для поиска:', reply_markup: buttons)
+    when 'Rutor'
+      bot.api.send_message(chat_id: message.chat.id, text: rutor)
     else
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+      nil
     end
   end
 end
