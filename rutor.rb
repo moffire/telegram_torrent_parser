20e1506f93cfd6eda597b05175fa57d26178b6c4
moffire <alexeydudarev.spb@gmail.com>
2019-11-19 22:29:11 +0300

    small changes

--- rutor.rb
+++ rutor.rb
@@ -5,20 +5,28 @@ class Rutor
 
   def self.find_torrents(search_word)
     full_info  = []
-    link       = "http://new-rutor.org/search/#{search_word}/"
+    link       = "http://new-rutor.org/search/#{URI.encode(search_word)}/"
     html       = open(link)
-    doc        = Nokogiri::HTML.parse(html, encoding = 'utf-8')
+    doc        = Nokogiri::HTML.parse(html, encoding='utf-8')
     table_rows = doc.css('div#index table tr')
 
     table_rows.drop(1).each do |row|
-      torrent_name     = row.css('td a')[1].text
-      torrent_link     = 'new-rutor.org' + row.css('td a')[0]['href']
-      torrent_size     = row.css('td[align="right"]')[1].text
       torrent_seeds    = row.css('td[align="center"] span.green').text[1..-1] #remove invisible space
       torrent_leechers = row.css('td[align="center"] span.red').text[1..-1] #remove invisible space
+
       if (torrent_seeds && torrent_leechers) != '0'
+        begin
+          torrent_name = row.css('td a')[1].text
+          torrent_link = 'new-rutor.org' + row.css('td a')[0]['href']
+        rescue NoMethodError
+          torrent_name = row.css('td a')[0].text
+          torrent_link = "Ссылка не торрент сломана(("
+        end
+
+        torrent_size = row.css('td[align="right"]')[1].text
         full_info << ["S: #{torrent_seeds} L: #{torrent_leechers} Size: #{torrent_size}", torrent_name, torrent_link]
       end
+
     end
 
     # format text in separate lines and return limited numbers of lines in order to avoid Error 400: message is too long
@@ -27,5 +35,7 @@ class Rutor
     else
       full_info.each_slice(10).map { |element| element.map { |a, s, d| [a, s, ["#{d}\n"]] } * "\n" }
     end
+
   end
+
 end
